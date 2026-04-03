531b0d54e649275a0515ef5602e00a5e99dbf4e5
BigOrangeBaby <39802306+chenhui0402@users.noreply.github.com>
2026-04-03 16:55:36 +0800

    Update lv_conf.h

--- lib/lv_conf.h
+++ lib/lv_conf.h
@@ -1052,7 +1052,7 @@ extern void *mp_lv_roots;
 
 /*Drivers for LCD devices connected via SPI/parallel port*/
 #define LV_USE_ST7735        0
-#define LV_USE_ST7789        0
+#define LV_USE_ST7789        1
 #define LV_USE_ST7796        0
 #define LV_USE_ILI9341       0
 
